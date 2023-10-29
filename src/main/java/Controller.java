57f4bda0f1df8e4f19eb6d785b6e5f292b7739ff
Gerardo <gerardoleoquin@gmail.com>
2023-10-29 19:11:09 +0000

    Eleventh commit

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -2,16 +2,12 @@ import matrixmultiplication.MatrixMultiplication;
 import matrixmultiplication.matrix.CCS;
 import matrixmultiplication.matrix.COO;
 import matrixmultiplication.matrix.CRS;
-import matrixmultiplication.matrix.Coordinate;
 import matrixmultiplication.matrixbuilders.CompressedColMatrixBuilder;
 import matrixmultiplication.matrixbuilders.CompressedRowMatrixBuilder;
 import matrixmultiplication.matrixbuilders.CooMatrixBuilder;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class Controller {
-    private final String path = "C:\\Users\\gerar\\OneDrive\\Escritorio\\ULPGC\\BD\\arc130.mtx";
+    private final String path = "Introduce the path to your .mtx files";
     private final CooMatrixBuilder cooMatrixBuilder = new CooMatrixBuilder(path);
     private final COO coo = cooMatrixBuilder.getCooMatrix();
     private final CompressedRowMatrixBuilder compressedRowMatrixBuilder = new CompressedRowMatrixBuilder(coo);
